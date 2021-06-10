aa4bfb605af6715bfbb3ab09a4b38e93695188d6
KooDH <76725137+KooDH@users.noreply.github.com>
2021-06-10 23:03:44 +0900

    자바기초 ( 예외처리 - 6.10 )

--- pjtTest/src/pjtTest/MainClass.java
+++ pjtTest/src/pjtTest/MainClass.java
@@ -4,13 +4,25 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		ChildClass child = new ChildClass(); // 자식 클래스 생성
-		ParentClass parent = new ParentClass();
+		int i = 10;
+		int j = 0;
+		int r = 0;
 		
-		child.childFun(); 	// 자식 메서드 호출
-		child.parentFun(); 	//자식 클래스를 생성했지만. 부모클래스를 상속 받았기 때문에
-							//부모 메서드까지 호출할 수 있음.
+		System.out.println("Exception BEFORE");
+		
+		try {
+			r = i / j; //예외가 처리될만한 코드를 안에 넣어줌
+		} catch (Exception e) { //예외처리를 해두게 되면 
+								// r = i / j코드는 실행 안되지만
+								// 아래의 msg코드는 실행이 됌
+			e.printStackTrace();
+			String msg = e.getMessage();
+			System.out.println("msg : " + msg);
+		}
 		
 		
-	}//결곽 값은 자식 클래스를 호출 했지만, 부모 클래스 먼저 호출된것을 확인할 수 있다.
+//		r = i / j; //예외 처리가 필요하며, AFTER는 출력이 안됌
+		
+		System.out.println("Exception AFTER");
+	}
 }
